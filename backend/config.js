512fe287898f78e4bd3b0e095532eb50cfd9d517
xinhuey <xinhuey.wong@gmail.com>
2024-05-29 21:04:33 -0400

    set up the book schema, and the routes to post and get a single book data

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL=
+    'mongodb+srv://user:user@virtual-bookstore.rjl97co.mongodb.net/?retryWrites=true&w=majority&appName=Virtual-Bookstore'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,70 @@
-import express from "express";
-import {PORT} from "./config.js"
+import express, { request } from "express";
+import {PORT, mongoDBURL} from "./config.js"
+import mongoose from "mongoose";
+import {Book} from './models/bookModel.js';
 
 const app = express();
 
-app.listen(PORT,() =>{
-    console.log(`App is listening to port: ${PORT}`);
+//Middleware for parsing request body 
+app.use(express.json());
+
+//get resource from the server
+app.get('/', (request, response)=>{
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack Tutorial');
+});
+
+//Route for Save a new book
+app.post('/books', async(request, response) =>{
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook ={
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: 'Error in Saving Book'});
+    }
+    
+});
+
+//Route for get all books from database
+app.get('/books', async(request, response) =>{
+    try{
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: 'Error in Fetching Books'});
+    }
 })
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('App connected to database');
+        app.listen(PORT,() =>{
+            console.log(`App is listening to port: ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
