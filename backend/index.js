f10f46bea27b5d0d79c4da5b8162b52f1d06b6ca
xinhuey <xinhuey.wong@gmail.com>
2024-05-29 22:08:07 -0400

    issue with useRoute, need to fix in frontend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,26 @@ import express, { request } from "express";
 import {PORT, mongoDBURL} from "./config.js"
 import mongoose from "mongoose";
 import {Book} from './models/bookModel.js';
-
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 const app = express();
 
 //Middleware for parsing request body 
 app.use(express.json());
+
+//Middleware for handling CORS policy
+//Option 1: Allow All Origins with default of cors(*)
+app.use(cors());
+
+//Option 2: Allow custom origins
+// app.use(
+//     cors({
+//         origin:'http://localhost:3000',
+//         methods: ['GET','POST','PUT','DELETE'],
+//         allowedHeaders: ['Content-Type'],
+
+//     })
+// )
 
 //get resource from the server
 app.get('/', (request, response)=>{
@@ -14,108 +29,7 @@ app.get('/', (request, response)=>{
     return response.status(234).send('Welcome to MERN Stack Tutorial');
 });
 
-//Route for Save a new book
-app.post('/books', async(request, response) =>{
-    try{
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ){
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-        const newBook ={
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500).send({message: 'Error in Saving Book'});
-    }
-    
-});
-
-//Route for get all books from database
-app.get('/books', async(request, response) =>{
-    try{
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        });
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500).send({message: 'Error in Fetching Books'});
-    }
-})
-
-//Route for get one book from database
-app.get('/books/:id', async(request, response) =>{
-    try{
-        const {id} = request.params;
-        const book = await Book.findById(id);
-        return response.status(200).json(book);
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500).send({message: 'Error in Fetching Book'});
-    }
-})
-
-//Route to update the book 
-app.put('/books/:id', async(request, response) =>{
-    try{
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ){
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-        const {id}= request.params;
-
-        const result = await Book.findByIdAndUpdate(id, request.body);
-
-        if(!result){
-            return response(404).json({message:'Book not found'});
-        }
-
-        return response.status(200).send({message:'Book Updated Successfully'});
-
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500).send({message: 'Error in Updating Book'});
-    }
-})
-
-//Route to delete the book 
-app.delete('/books/:id', async(request, response) => {
-    try{
-        const{id} = request.params;
-        const result = await Book.findByIdAndDelete(id);
-
-        if(!result){
-            return response.status(404).json({message: 'Book not found'});
-        }
-
-        return response.status(200).send({message:'Book deleted successfully'});
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-})
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+import createBook from './pages/createBook';
+import showBook from './pages/showBook';
+import editBook from './pages/editBook';
+import deleteBook from './pages/deleteBook';
+
 
 export const App = () => {
   return (
-    <div className='bg-red-400 text-white' >App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<createBook />} />
+      <Route path="/books/details/:id" element={<showBook />} />
+      <Route path="/books/edit/:id" element={<editBook />} />
+      <Route path="/books/delete/:id" element={<deleteBook />} />
+    </Routes>
   )
 }
 
