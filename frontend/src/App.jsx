3f03aae9a252c322a5d4021139b99bbf863bd31a
xinhuey <xinhuey.wong@gmail.com>
2024-05-30 20:05:55 -0400

    fixed the home page and created showbook.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import Home from './pages/home';
-import createBook from './pages/createBook';
-import showBook from './pages/showBook';
-import editBook from './pages/editBook';
-import deleteBook from './pages/deleteBook';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/books/create" element={<createBook />} />
-      <Route path="/books/details/:id" element={<showBook />} />
-      <Route path="/books/edit/:id" element={<editBook />} />
-      <Route path="/books/delete/:id" element={<deleteBook />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   )
 }
--- frontend/src/pages/createBook.jsx
+++ frontend/src/pages/createBook.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const createBook = () => {
+const CreateBook = () => {
+  const [title, setTitle] = useState('');
+  const[author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading]= useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook(()=>{
+    const data ={
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios 
+      .post('http://localhost:5555/books', data)
+      .then(() =>{
+        setLoading(false);
+        navigate('/');
+
+      })
+      .catch((error)=>{
+        setLoading(false);
+        alert('An error has occurred. Please check console');
+        console.log(error);
+      });
+  });
   return (
     <div>createBook</div>
   )
 }
 
-export default createBook
+export default CreateBook
